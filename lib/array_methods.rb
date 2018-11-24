e22eaf492da07916334bb4dca6ec1a72594373aa
Stephanie Messad <sabud1208@gmail.com>
2018-11-24 05:07:02 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def using_include(array, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-if array.include?("element")
-  false
-else
+if array.include? == ("element")
   true
+else
+false
 end
 end
 
